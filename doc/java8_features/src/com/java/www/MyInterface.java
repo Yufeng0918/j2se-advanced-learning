334629bca4e8610baaea77b37828a966dc4bb407
daiyu <jiangyufeng.0918@gmail.com>
2020-09-05 00:19:07 +0800

    java8 复习

--- doc/java8_features/src/com/java/www/MyInterface.java
+++
@@ -1,7 +0,0 @@
-package com.java.www;
-
-public interface MyInterface {
-    void walk();
-
-    String speak(String s);
-}
--- doc/java8_features/src/com/java/www/MyInterface2.java
+++
@@ -1,7 +0,0 @@
-package com.java.www;
-
-@FunctionalInterface
-public interface MyInterface2 {
-
-    void getDes();
-}
